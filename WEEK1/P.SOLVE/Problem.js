0d51dfcf129724261fcc27f9883934bc43f2ea66
TAIJULAMAN <taijulaman8@gmail.com>
2024-01-06 22:12:41 +0600

    week 1

--- WEEK1/P.SOLVE/Problem.js
+++ WEEK1/P.SOLVE/Problem.js
@@ -1,38 +1,41 @@
 // star pattern 1:
-for(var i=1;i<=5;i++){
-    let row ="";
-    for(var j=1;j<=i;j++){
-        row +="*";
+for(let i=1;i<=5;i++){
+    let row ='';
+    for(let j=1;j<=i;j++){
+        row +='*';
     }
     console.log(row);
 }
+
+
 // star pattern 2:
-for(var i=1;i<=5;i++){
-    let row ="";
-    for(var j=5;j>i;j--){
-        row +="";
+for(let i=1;i<=5;i++){
+    let row1 ="";
+    for(let j=5;j>i;j--){
+        row1 +="_";
     }
-    for(var k=1;k<=i;k++){
-        row +="*";
+    for(let k=1;k<=i;k++){
+        row1 +="*";
     }
-    console.log(row);
+    console.log(row1);
 }
 // star pattern 3:
-for(var i=5;i>=1;i--){
-    let row ="";
-    for(var j=1;j<=i;j++){
-        row +="*";
+for(let i=5;i>=1;i--){
+    let row3 ="";
+    for(let j=1;j<=i;j++){
+        row3 +="*";
     }
-    console.log(row);
+    console.log(row3);
 }
-// star pattern 2:
-for(var i=5;i<=1;i--){
-    let row ="";
-    for(var j=5;j>i;j--){
-        row +="";
+
+// star pattern 4:
+for(let i=5;i>=1;i--){
+    let row4 ="";
+    for(let j=5;j>i;j--){
+        row4 +="_";
     }
-    for(var k=1;k<=i;k++){
-        row +="*";
+    for(let k=1;k<=i;k++){
+        row4 +="*";
     }
-    console.log(row);
+    console.log(row4);
 }
